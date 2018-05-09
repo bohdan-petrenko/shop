66693cb01847fcbcebdf38a7c7a99d51b59d2c96
Katya <dovgakatee@gmail.com>
2018-05-09 13:23:34 +0300

    with users save and update

--- src/main/java/com/kate/shop/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/kate/shop/repository/impl/UserRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.kate.shop.repository.impl;
 
-import com.kate.shop.entity.Role;
 import com.kate.shop.entity.User;
 import com.kate.shop.repository.UserRepository;
 import com.kate.shop.utils.DaoUtils;
@@ -13,14 +12,11 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
